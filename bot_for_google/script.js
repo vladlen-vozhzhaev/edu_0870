63c2af61a57f58920d4e4459c01d0d6e34f8b8be
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-09-11 10:02:59 +0300

    11.09.2020
    1) Ключевое слово набирается по буквам.
    2) Сделали задержку по времени между действиями скрипта.
    3) Скрипт теперь работает на искомом сайте.
    4) Скрипт умеет искать сайт на разных страницах поисковика до 10 страницы включительно.
    5) Скрипт переходит по случайным ссылкам на искомом сайте
    6) С вероятностью 20% возвращается на сайт google.com

--- bot_for_google/script.js
+++ bot_for_google/script.js
@@ -5,25 +5,52 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.google.com/*
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
 
-let keywords = ['Гобой','Как звучит флейта', 'Кларнет'];
+let googleInput = document.getElementsByName('q')[0];
+let keywords = ['Гобой','Как звучит флейта', 'Кларнет','Саксофон','Тромбон','Валторна'];
 let keyword = keywords[getRandom(0,keywords.length)];
 let btnK = document.getElementsByName('btnK')[0];
+let i =0;
+let links = document.links;
 
 if (btnK != undefined){
-    document.getElementsByName('q')[0].value = keyword;
-    document.getElementsByName('btnK')[0].click();
+    let timerId = setInterval(()=>{
+        googleInput.value += keyword[i];
+        i++;
+        if (i==keyword.length){
+            clearInterval(timerId);
+            btnK.click();
+        }
+    },1000);
+}else if(location.hostname == "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai"){
+    setInterval(()=>{
+        let index = getRandom(0,links.length);
+        if (getRandom(0,101)>=80){
+            location.href = 'https://www.google.com/';
+        }
+        else if (links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1)
+            links[index].click();
+    },getRandom(3000,7000));
 }else{
-    let links = document.links;
-
+    let nextGooglePage = true;
     for(let i=0; i<links.length; i++){
         if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
-            console.log("Ссылка найдена "+links[i]);
-            links[i].click();
+            let link = links[i];
+            nextGooglePage = false;
+            setTimeout(()=>{link.click();},getRandom(1000,4000));
             break;
         }
+    }
+    if (document.querySelector('.YyVfkd').innerText=="10"){
+        nextGooglePage = false;
+        location.href = 'https://www.google.com/';
+    }
+
+    if (nextGooglePage){
+        setTimeout(()=>{pnnext.click();},getRandom(1000,4000));
     }
 }
 
